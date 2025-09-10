413196fab7469c15edd957adedd075df00d32d9c
Caden McArthur <caden.mcarthur@gmail.com>
2025-09-09 20:33:42 -0500

    build

--- App.py
+++
@@ -1,5 +0,0 @@
-#make some endpoints to track unique users and statistics i can view
-# not ultra priority for stats but it would be nice to look at
-import flask
-
-
---
+++ Server/App.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, send_from_directory
+
+app = Flask(__name__, static_folder="../client/dist", static_url_path="/")
+
+@app.route('/')
+def serve_dist():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route('/api/hello')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/api/uniqueVisitor/', methods=['POST'])
+def uniqueVisitor():
+    data = data.from_json(request.json)
+    return 'unique user'
+
+if __name__ == '__main__':
+    app.run(debug=True)
