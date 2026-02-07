c1e2ef0381dabfdc57b4cf8eb7387ef8d754a235
cmac2112 <caden.mcarthur@gmail.com>
2026-02-07 14:03:10 -0600

    removed lllm comment for now

--- client/src/Components/ProgressionMeter/RenderContent/Intro.tsx
+++ client/src/Components/ProgressionMeter/RenderContent/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
     <div>
       <p>Here you will find more in depth descriptions of all of my experience and projects!</p>
       <p>Use the handy progress bar on the right side to browse the sections!</p>
-      <p>If you want to summarize this page using an LLM of your choice. <Emphasis>Click Here</Emphasis> to copy all of the content into a LLM friendly format</p>
+      {/*<p>If you want to summarize this page using an LLM of your choice. <Emphasis>Click Here</Emphasis> to copy all of the content into a LLM friendly format</p> */}
     </div>
   )
 }
